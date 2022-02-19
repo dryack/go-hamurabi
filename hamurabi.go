54c7d60d613131f865dd28fbff374844aefb3147
dryack <dave.ryack@gmail.com>
2022-02-19 08:14:27 -0800

    Prep for automated play

--- hamurabi.go
+++ hamurabi.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"io"
 	"math"
 	"math/rand"
+	"os"
 	"time"
 )
 
+var reader io.Reader
+var writer io.Writer
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	gameTurns := playerInput("How many turns would you like to play?", 10, math.MaxInt, "")
-	sumer := initCityState(gameTurns)
-	orientation()
+	var gameTurns int
+	var test bool
 
+	if os.Args[0] == "-test" {
+		gameTurns = 100
+		test = true
+	} else {
+		gameTurns = playerInput("How many turns would you like to play?", 10, math.MaxInt, "")
+		test = false
+	}
+	sumer := initCityState(gameTurns)
+
+	if !test {
+		orientation()
+		for t := 0; t <= sumer.turns; t++ {
+			printYearResults(sumer)
+			getAcres(sumer)
+			technology(sumer)
+			feedPeople(sumer)
+			agriculture(sumer)
+		}
+		endOfReign(sumer)
+		os.Exit(0)
+	}
 	for t := 0; t <= sumer.turns; t++ {
 		printYearResults(sumer)
 		getAcres(sumer)
@@ -20,4 +45,5 @@ func main() {
 		agriculture(sumer)
 	}
 	endOfReign(sumer)
+
 }
