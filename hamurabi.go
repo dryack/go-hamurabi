be5f54615baa098cd001a2ac49d9fdc03e97fb59
dryack <dave.ryack@gmail.com>
2022-02-20 13:04:37 -0800

    Basic TUI work started

--- hamurabi.go
+++ hamurabi.go
@@ -1,13 +1,69 @@
 package main
 
 import (
+	"context"
+	"github.com/mum4k/termdash"
+	"github.com/mum4k/termdash/container"
+	"github.com/mum4k/termdash/linestyle"
+	"github.com/mum4k/termdash/terminal/tcell"
+	"github.com/mum4k/termdash/terminal/terminalapi"
+	"github.com/mum4k/termdash/widgets/text"
 	"math/rand"
 	"os"
 	"time"
 )
 
 func main() {
-	gameLoop()
+	term, err := tcell.New()
+	if err != nil {
+		panic(err)
+	}
+	defer term.Close()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	gameWidget, err := text.New(text.RollContent(), text.WrapAtWords())
+	if err != nil {
+		panic(err)
+	}
+	resourceWidget, err := text.New(text.RollContent(), text.WrapAtWords())
+	if err != nil {
+		panic(err)
+	}
+
+	cont, err := container.New(term,
+		container.Border(linestyle.Light),
+		container.BorderTitle("Hamurabi -- Press Q to Quit"),
+		container.BorderTitleAlignCenter(),
+		container.SplitHorizontal(
+			container.Top(
+				container.Focused(),
+				container.Border(linestyle.Light),
+				container.BorderTitle("Gameplay"),
+				container.PlaceWidget(gameWidget),
+			),
+			container.Bottom(
+				container.Border(linestyle.Light),
+				container.BorderTitle("Resources"),
+				container.PlaceWidget(resourceWidget),
+			),
+			container.SplitPercent(85)),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	quitter := func(k *terminalapi.Keyboard) {
+		if k.Key == 'q' || k.Key == 'Q' {
+			cancel()
+		}
+	}
+
+	if err := termdash.Run(ctx, term, cont, termdash.KeyboardSubscriber(quitter)); err != nil {
+		panic(err)
+	} else {
+		gameLoop()
+	}
 }
 
 func gameLoop() {
