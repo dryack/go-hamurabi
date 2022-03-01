616b06d077bfe1032b9d9946423d742ab8574645
dryack <dave.ryack@gmail.com>
2022-02-28 17:38:42 -0800

    Initial pass at scoring

--- endOfReign.go
+++ endOfReign.go
@@ -12,7 +12,45 @@ func (s *gameSession) endOfReign() {
 	fmt.Printf("You ordered the building of %d granaries during your rule.\n", s.state.granary)
 	fmt.Printf("The city began with 10 acres per person and ended with %d.\n", s.state.acres/s.state.population)
 	fmt.Printf("We maintained a herd of %d cows.\n", s.state.cows)
+	fmt.Printf("Your final score was %d\n", s.calcScore())
 	fmt.Printf("\tAvg Bushels at turn start: %d; Avg Bushels eaten by rats: %d\n", s.avgBushelsAvail/s.turns, s.avgPestEaten/s.turns) // DEBUG
 
 	os.Exit(0)
+}
+
+func (s *gameSession) calcScore() int {
+	var (
+		population = 1
+		cows       = 5
+		land       = 10
+		plows      = 2
+		palace1    = 200
+		palace2    = 1000
+		palace3    = 5000
+		granary    = 15
+	)
+	score := (population * s.state.population) +
+		(cows * s.state.cows) +
+		(land * s.state.acres) +
+		(plows * s.state.plows) +
+		(granary * s.state.granary) +
+		(palace1 * func(p bool) int {
+			if p {
+				return 1
+			}
+			return 0
+		}(s.state.palace1)) +
+		(palace2 * func(p bool) int {
+			if p {
+				return 1
+			}
+			return 0
+		}(s.state.palace2)) +
+		(palace3 * func(p bool) int {
+			if p {
+				return 1
+			}
+			return 0
+		}(s.state.palace3))
+	return score
 }
